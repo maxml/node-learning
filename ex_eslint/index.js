9f3cef3f73371725709f5a14ce4410e1dee269c1
maxml <maxml@inbox.ru>
2020-05-14 18:26:01 +0300

    new servers && linter && new mode for axios

---
+++ ex_eslint/index.js
@@ -0,0 +1,602 @@
+var foo = {
+    bar: 1,
+};
+
+if (foo.bar = 0) {
+    console.log(foo.bar);
+}
+
+var controlRegexPatternA = /\\x1f/;
+var controlRegexPatternB = new RegExp('\x20');
+
+debugger;
+
+
+// function hoge(a, b, a) {
+//     return a;
+// }
+
+hoge(1, 2, 3);
+
+var obj = {
+    a: 1,
+    b: 2,
+    a: 1
+};
+
+switch (obj.a) {
+    case 1:
+        break;
+    case 2:
+        break;
+    case 1:
+        break;
+    default:
+        break;
+}
+
+var charClassPattern = /^[]/;
+
+if (obj.a) {
+}
+
+try {
+} catch (e) {
+    e = 0;
+}
+
+if (!!obj.a) {
+}
+
+var extraParens = (1 * 0)
+
+function extraSemi() {
+};
+
+function funcAssignA() {
+}
+
+function funcAssignB() {
+}
+
+funcAssignA = funcAssignB;
+
+if (obj.a) {
+    function innerDeclaration() { }
+}
+
+
+var invalidPattern = RegExp('.', 'z');
+
+
+if ((!a in obj)) {
+    console.log(0);
+}
+
+Math();
+
+var spaceInText = /foo         bar/;
+
+var reservedKeys = {
+    class: ''
+};
+
+var sparseArray = [, ,];
+
+function unreachable() {
+    var x = 0;
+
+    return x;
+
+    x = 1;
+}
+
+if (obj.a == NaN) {
+    obj.a = 0;
+}
+
+/**
+ * valid JSDoc
+ */
+function validJSDoc() {
+    return 'valid';
+}
+
+typeof "valid typeof" === "strin";
+
+
+var accessorPairs = {
+    set foo(val) {
+        this.val = val;
+    }
+};
+
+
+function blockScoped() {
+    if (obj.a) {
+        var blockScopedVar = "var";
+    }
+
+    console.log(blockScopedVar);
+}
+
+
+function complexity(opt) {
+    if (opt) {
+        return opt;
+    } else if (!opt) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+function consistentReturn(x) {
+    if (x) {
+        return x;
+    } else {
+        return;
+    }
+}
+
+if (obj) console.log(obj);
+
+switch (obj.a) {
+    case 1:
+        break;
+    case 2:
+        break;
+}
+
+var dotNotation = obj['a'];
+
+var dot = {
+    location: function () {
+        return this;
+    }
+};
+
+dot.
+    location();
+
+dot
+    .location();
+
+if (obj.a == 0) {
+    console.log(obj.b);
+}
+
+for (key in obj) {
+    console.log(key);
+}
+
+alert(this);
+
+function noArgumentsCall() {
+    var arg = arguments.callee;
+}
+
+function lookLikeDivision() {
+    return /=foo/;
+}
+
+function elseReturn(x) {
+    if (x) {
+        return x;
+    } else {
+        return;
+    }
+}
+
+emptyLabel:
+var foobar = 0;
+
+if (obj == null) {
+    console.log('null');
+}
+
+eval();
+
+String.prototype.extendNative = function () {
+    return this;
+};
+
+var extraBind = function () {
+    console.log('extra');
+}.bind(hoge);
+
+
+switch (obj.a) {
+    case 1:
+        obj.c = 100;
+    default:
+        obj.c = null;
+}
+
+var floatingDecimal = .5;
+
+setTimeout("console.log('implied!')", 100);
+
+
+function DisallowIterator() {
+}
+DisallowIterator.prototype.__iterator__ = function () {
+};
+
+
+labeldStatement:
+break labeldStatement;
+
+{ }
+
+for (var i = 0; i < 10; i++) {
+    (function (n) {
+        console.log(n);
+    })(i);
+}
+
+var multispaces = 1;
+
+var multilineString = 'line\
+    line';
+
+String = 'native object';
+
+var funcConstructor = new Function();
+
+var primitiveWrapper = new String('primitive');
+
+function NoNew() {
+}
+
+new NoNew();
+
+// var octalEscape = '\251';
+
+// var octalLiteral = 011;
+
+var reassignParam = function (x) {
+    x = 0;
+};
+
+var env = process.env;
+
+var proto = obj.__proto__;
+
+var redeclearVar = 0;
+var redeclearVar = 1;
+
+function returnAssignment() {
+    var assigned = 0;
+
+    return assigned = 1;
+}
+
+location.href = 'javascript:void(0)';
+
+var selfCompare = 10;
+
+if (selfCompare === selfCompare) {
+    selfCompare = 0;
+}
+
+
+var noSequenceA = (3, 5);
+noSequenceA = noSequenceB += 5, noSequence + noSequenceB;
+
+function throwLiteral() {
+    throw "no-throw-literal";
+}
+
+"unused expressions";
+
+void 0;
+
+// TODO: no-warning-comments
+
+// with (obj) {
+//     console.log(this);
+// }
+
+var radix = parseInt("001");
+
+function varsOnTop(n) {
+    var vars1 = 0;
+    if (n) {
+        return n;
+    }
+
+    var vars2 = 10;
+
+    return vars1 + vars2 + n;
+}
+
+var wrapIife = function () {
+    return {};
+}();
+
+
+var yoda = 1;
+
+if (1 === yoda) {
+    console.log(yoda);
+}
+
+"use strict";
+
+function useStrict() {
+    "use strict";
+}
+
+var shadowErr = 'err';
+try {
+    throw new Error();
+} catch (shadowErr) {
+}
+
+var deleteVar;
+// delete deleteVar;
+
+var labelVar = 0;
+function bar() {
+    labelVar:
+    for (; ;) {
+        break labelVar;
+    }
+}
+
+var undefined;
+
+var shadow;
+
+function noShadow() {
+    var shadow = 'shadow';
+}
+
+var undefInit = undefined;
+
+var unusedVar;
+
+console.log(beforeDefine);
+var beforeDefine = 0;
+
+function handleCbErr(err, done) {
+    done();
+}
+
+var mixedRequire = require('./module'),
+    mixed = 'mixed';
+
+var NewRequire = new require('./module');
+
+var pathConcat = __dirname + 'path.js';
+
+function processExit() {
+    process.exit(1);
+}
+
+var fs = require('fs');
+
+fs.existsSync('./some-path');
+
+var bracketSpaces = [1, 2];
+
+if (obj) {
+    obj.d = 0;
+}
+else {
+    obj = {};
+}
+
+var camel_case = '';
+
+var space = 0, arround = 1;
+
+var comma
+    , style;
+
+var computedProp = { foo: 1 };
+
+computedProp['foo'];
+
+var self;
+
+setTimeout(function () {
+    self = this;
+    console.log(self);
+}, 0);
+
+var funcNames = function () { };
+
+function funcStyleA() { }
+
+var funcStyleB = function () { };
+
+var keySpacing = {
+    space: []
+};
+
+var linesAroundCommentA;
+// lines around comment
+var linesAroundCommentB;
+
+
+function nestedCb(cbA) {
+    cbA(function (cbB) {
+        cbB(function (cbC) {
+            cbC(function (cbD) {
+                cbD(function () {
+                });
+            });
+        });
+    });
+}
+
+function newCap() { }
+
+var cap = new newCap();
+
+function NewParens() { }
+
+var parens = new NewParens;
+
+var newLineAfterVar = 0;
+console.log(newLineAfterVar);
+
+var arrayConstructor = new Array(1, 2);
+
+for (var i = 0; i < 10; i++) {
+    if (i < 5) {
+        continue;
+    }
+}
+
+var inlineComment = '';//inline
+
+if (obj.a) {
+} else {
+    if (obj.b) {
+    }
+}
+
+
+
+var nestedTernary = obj.a ? 0 : obj.b ? 1 : 2;
+
+var NewObj = new Object();
+
+var spacedFn = function () { };
+
+spacedFn();
+
+var noTernary = obj ? 1 : 0;
+
+var trailingspaces;
+
+var _underscoreDangle;
+
+var unneededTernary = obj.a === 1 ? true : false;
+
+var curlySpace = { foo: 1 };
+
+function oneVar() {
+    var oneVarA;
+    var oneVarB;
+}
+
+var operatorAssignment = 1;
+
+operatorAssignment = operatorAssignment + 1;
+
+var operatorLinebreak = 0 +
+    1;
+
+function paddedBlock() {
+
+    console.log(this);
+}
+
+var quote = {
+    prop: ''
+};
+
+var quotes = "quotes quotes";
+
+var semiSpacing;
+
+var semi
+
+var sortVarsA, sortVarsC, sortVarsB;
+
+if (obj) {
+    console.log('space after keywords');
+}
+
+if (obj) {
+    console.log('space before blocks');
+}
+
+function spaceBeforeFnParen() { }
+
+(function () {
+    console.log('space in parens');
+})();
+
+var spaceInfixOps = 1 + 1;
+
+function spaceReturnThrowCase() {
+    return -1;
+}
+
+++obj.a;
+
+//spaced comment
+
+function wrapRegex() {
+    return /regex/.test('regex');
+}
+
+class MyClass {
+
+    constructor() {
+        // super();
+    }
+}
+
+function* generators() {
+}
+
+class BeforeSuper extends MyClass {
+    constructor() {
+        this.name = '';
+        super();
+    }
+}
+
+var noVar;
+
+var objShorthand = {
+    a: function () {
+    }
+};
+
+let preferConst = 0;
+
+function maxDepth(x, y) {
+    for (var i = 0; i < 10; i++) {
+        if (x) {
+            if (y) {
+                i++;
+            }
+        }
+    }
+}
+
+var maxLen = 'maxlenmaxlenmaxlenmaxlenmaxlenmaxlenmaxlenmaxlenmaxlenmaxlenmaxlenmaxlenmaxlenmaxlenmaxlenmaxlenmaxlenmaxlenmaxlenmaxlenmaxlenmaxlenmaxlenmaxlenmaxlenmaxlenmaxlenmaxlenmaxlenmaxlenmaxlenmaxlenmaxlenmaxlenmaxlenmaxlenmaxlenmaxlenmaxlenmaxlenmaxlen';
+
+function maxParams(a, b, c, d, e) {
+}
+
+function maxStatements() {
+    var hogehoge = 1;
+    var foobar = 2;
+    var barfoo = 3;
+    var fooo = 4;
+}
+
+var bitwise = obj.a ^ obj.b;
+
+var plusplus = 0;
+
+plusplus++;
+
+/* eslint-disable */
+alert("test");
+/* eslint-enable */
+
+alert("test"); // eslint-disable-line
+
+/* eslint-disable no-alert, no-console */
+alert("test");
+console.log("test");
+/* eslint-enable no-alert, no-console */
---
+++ practice/practice20.js
@@ -0,0 +1,308 @@
+function naming() {
+    const yyyymmdstr = moment().format('YYYY/MM/DD');
+
+    const Car = {
+        carMake: 'Honda',
+        carModel: 'Accord',
+        carColor: 'Blue'
+    };
+
+    function paintCar(car) {
+        car.carColor = 'Red';
+    }
+
+    const DAYS_IN_WEEK = 7;
+    const daysInMonth = 30;
+
+    const songs = ['Back In Black', 'Stairway to Heaven', 'Hey Jude'];
+    const Artists = ['ACDC', 'Led Zeppelin', 'The Beatles'];
+
+    function eraseDatabase() { }
+    function restore_database() { }
+
+    class animal { }
+    class Alpaca { }
+}
+
+// getUserInfo();
+// getClientData();
+// getCustomerRecord();
+
+function constants() {
+    setTimeout(blastOff, 86400000);
+
+    const address = 'One Infinite Loop, Cupertino 95014';
+    const cityZipCodeRegex = /^[^,\\]+[,\\\s]+(.+?)\s*(\d{5})?$/;
+    const [, city, zipCode] = address.match(cityZipCodeRegex) || [];
+    saveCityZipCode(city, zipCode);
+}
+
+function params() {
+    // objects, array
+    function createMenu(title, body, buttonText, cancellable) {
+        // ...
+    }
+}
+
+function emailClients(clients) {
+    clients.forEach((client) => {
+        const clientRecord = database.lookup(client);
+        if (clientRecord.isActive()) {
+            email(client);
+        }
+    });
+}
+
+function commonFunctionality() {
+    function showDeveloperList(developers) {
+        developers.forEach((developer) => {
+            const expectedSalary = developer.calculateExpectedSalary();
+            const experience = developer.getExperience();
+            const githubLink = developer.getGithubLink();
+            const data = {
+                expectedSalary,
+                experience,
+                githubLink
+            };
+
+            render(data);
+        });
+    }
+
+    function showManagerList(managers) {
+        managers.forEach((manager) => {
+            const expectedSalary = manager.calculateExpectedSalary();
+            const experience = manager.getExperience();
+            const portfolio = manager.getMBAProjects();
+            const data = {
+                expectedSalary,
+                experience,
+                portfolio
+            };
+
+            render(data);
+        });
+    }
+}
+
+function defaultObject() {
+    const menuConfig = {
+        title: 'Order',
+        // Пользователь не добавил поле 'body'
+        buttonText: 'Send',
+        cancellable: true
+    };
+
+    function createMenu(config) {
+        config = Object.assign({
+            title: 'Foo',
+            body: 'Bar',
+            buttonText: 'Baz',
+            cancellable: true
+        }, config);
+
+        // config теперь равен: {title: "Order", body: "Bar", buttonText: "Send", cancellable: true}
+        // ...
+    }
+
+    createMenu(menuConfig);
+}
+
+function noFlags() {
+    function createFile(name, temp) {
+        if (temp) {
+            fs.create(`./temp/${name}`);
+        } else {
+            fs.create(name);
+        }
+    }
+}
+
+function global() {
+    class SuperArray extends Array {
+        diff(comparisonArray) {
+            const hash = new Set(comparisonArray);
+            return this.filter(elem => !hash.has(elem));
+        }
+    }
+}
+
+function extractConditions() {
+    function shouldShowSpinner(fsm, listNode) {
+        return fsm.state === 'fetching' && isEmpty(listNode);
+    }
+
+    if (shouldShowSpinner(fsmInstance, listNodeInstance)) {
+        // ...
+    }
+}
+
+
+function noConditions() {
+    class Airplane {
+        // ...
+        getCruisingAltitude() {
+            switch (this.type) {
+                case '777':
+                    return this.getMaxAltitude() - this.getPassengerCount();
+                case 'Air Force One':
+                    return this.getMaxAltitude();
+                case 'Cessna':
+                    return this.getMaxAltitude() - this.getFuelExpenditure();
+            }
+        }
+    }
+
+    class Airplane {
+        // ...
+    }
+
+    class Boeing777 extends Airplane {
+        // ...
+        getCruisingAltitude() {
+            return this.getMaxAltitude() - this.getPassengerCount();
+        }
+    }
+
+    class AirForceOne extends Airplane {
+        // ...
+        getCruisingAltitude() {
+            return this.getMaxAltitude();
+        }
+    }
+
+    class Cessna extends Airplane {
+        // ...
+        getCruisingAltitude() {
+            return this.getMaxAltitude() - this.getFuelExpenditure();
+        }
+    }
+}
+
+function move() {
+    function travelToTexas(vehicle) {
+        if (vehicle instanceof Bicycle) {
+            vehicle.pedal(this.currentLocation, new Location('texas'));
+        } else if (vehicle instanceof Car) {
+            vehicle.drive(this.currentLocation, new Location('texas'));
+        }
+    }
+}
+
+function gettersSetters() {
+    function makeBankAccount() {
+        // приватная переменная
+        let balance = 0;
+
+        // геттер является публичным, так как возвращается в объекте ниже
+        function getBalance() {
+            return balance;
+        }
+
+        // сеттер является публичным, так как возвращается в объекте ниже
+        function setBalance(amount) {
+            // ... валидация перед обновлением баланса
+            balance = amount;
+        }
+
+        return {
+            // ...
+            getBalance,
+            setBalance,
+        };
+    }
+
+    const account = makeBankAccount();
+    account.setBalance(100);
+}
+
+function privateFields() {
+    function makeEmployee(name) {
+        return {
+            getName() {
+                return name;
+            },
+        };
+    }
+
+    const employee = makeEmployee('John Doe');
+    console.log(`Employee name: ${employee.getName()}`); // Employee name: John Doe
+    delete employee.name;
+    console.log(`Employee name: ${employee.getName()}`); // Employee name: John Doe
+}
+
+function builder() {
+    class Car {
+        constructor() {
+            this.make = 'Honda';
+            this.model = 'Accord';
+            this.color = 'white';
+        }
+
+        setMake(make) {
+            this.make = make;
+            return this;
+        }
+
+        setModel(model) {
+            this.model = model;
+            return this;
+        }
+
+        setColor(color) {
+            this.color = color;
+            return this;
+        }
+
+        save() {
+            console.log(this.make, this.model, this.color);
+            return this;
+        }
+    }
+
+    const car = new Car()
+        .setColor('pink')
+        .setMake('Ford')
+        .setModel('F-150')
+        .save();
+}
+
+function error() {
+    try {
+        functionThatMightThrow();
+    } catch (error) {
+        console.error(error);
+        notifyUserOfError(error);
+        reportErrorToService(error);
+    }
+
+    getdata()
+        .then((data) => {
+            functionThatMightThrow(data);
+        })
+        .catch((error) => {
+            console.error(error);
+            notifyUserOfError(error);
+            reportErrorToService(error);
+        });
+}
+
+function comments() {
+    /**
+     * 2016-12-20: Удалены монады, не понимал их (RM)
+     * 2016-10-01: Улучшено использование специальных монады (JP)
+     * 2016-02-03: Исключена проверка типов (LI)
+     * 2015-03-14: Добавлен combine с проверкой типов (JR)
+     */
+    function combine(a, b) {
+        return a + b;
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////
+    // Создание объекта
+    ////////////////////////////////////////////////////////////////////////////////
+    $scope.model = {
+        menu: 'foo',
+        nav: 'bar'
+    };
+}
